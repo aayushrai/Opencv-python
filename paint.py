c267eeed21dda897c204f9ec67c46d324531453e
aayushrai <www.aayushrai@gmail.com>
2018-12-28 18:48:26 +0530

    learn contour

--- paint.py
+++ paint.py
@@ -2,16 +2,21 @@ import numpy as np
 import cv2
 
 page = np.ones([800,1000,3],"uint8")*255
+plus = cv2.imread("plusminus20.jpg")
 brush = False
 eraser = False
 pressed = False
 size = 5
-color = (0,0,0)
+color = (0,0,0) 
+ # Tools select block 
 page[20:40,20:40] =(0,0,0)      # brush black block
 page[20:40,60:80] =(0,0,0)     # eraser black block
 page[80:100,20:40] =(0,0,255)   # color red block
 page[110:130,20:40] =(0,255,0) #color green bolck
 page[140:160,20:40] =(255,0,0) #color blue block 
+page[180:200,20:40] = plus[0:20,0:20] #plus symbol block 
+page[220:240,20:40] = plus[0:20,20:40] # minus symbol block
+
 def click(event,y,x,flags,para):
     global page,brush,eraser,pressed,color,size
 
@@ -28,21 +33,29 @@ def click(event,y,x,flags,para):
             brush = False
             print("brush=", brush)
             print("eraser=",eraser)
-    
-    if event == cv2.EVENT_LBUTTONDOWN:
+ # Red color brush
+    if event == cv2.EVENT_LBUTTONDOWN:      
         if x in range(80,100) and y in range(20,40):
             color =(0,0,255)
-  
-    if event == cv2.EVENT_LBUTTONDOWN:
+ # Green color brush 
+    if event == cv2.EVENT_LBUTTONDOWN:                   
         if x in range(110,130) and y in range(20,40):
             color =(0,255,0)
-    
+ # blue color brush   
     if event == cv2.EVENT_LBUTTONDOWN:
         if x in range(140,160) and y in range(20,40):
             color =(255,0,0)
-
-
-
+ # Increase brush and eraser size           
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if x in range(180,200) and y in range(20,40):
+            size = size + 1
+            print("size:",size)
+ # Decrease brush and eraser size           
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if x in range(220,240) and y in range(20,40):
+            size = size - 1
+            print("size:",size)
+ # Drawing sheet
     if x in range(50,1000) and y in range(50,1000):         # on x axis and y axis area of color is 50 t0 1000 mean paint only in area 50 to 1000
         
     #    if event == cv2.EVENT_MOUSEMOVE:
@@ -68,6 +81,7 @@ def click(event,y,x,flags,para):
 
              elif event == cv2.EVENT_LBUTTONUP:
                 pressed = False
+                
 cv2.namedWindow("page")
 cv2.setMouseCallback("page",click)
 
