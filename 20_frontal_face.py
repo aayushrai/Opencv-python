e87ddbacaf9185586e820cc23058c061c6d8c272
aayushrai <www.aayushrai@gmail.com>
2018-12-30 20:20:25 +0530

    may e last commit

--- 20_frontal_face.py
+++ 20_frontal_face.py
@@ -3,15 +3,15 @@ import numpy as np
 
 img = cv2.imread("faces1.jpg",1)
 gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-path = "frontalFace10/haarcascade_frontalface_default.xml"
+path = "frontalFace10/haarcascade_frontalface_default.xml"   # this file dectect faces in image with help of machine learning
 
 face_cascade = cv2.CascadeClassifier(path)
 
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.10,minNeighbors=5,minSize=(40,40))
 print(len(faces))
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+for (x,y,w,h) in faces:    # classifier return coordinate of faces 
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)   # Draw rectangle on that coordinate
 
 cv2.imshow("image",img)
 cv2.waitKey(0)
